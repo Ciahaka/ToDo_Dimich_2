703577f5d3a99d52ce5604eebe4499576d28b7b6
Ciahaka <wilddiver.by@gmail.com>
2022-11-30 15:24:00 +0300

    add App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {TaskPropsType, Todolist} from './component/Todolist';
+import {v1} from 'uuid';
+
+
+export type SelectionType = 'All' | 'Completed' | 'Active'
 
 function App() {
+  let [tasks, setTasks] = useState<Array<TaskPropsType>>([
+    {id: v1(), title: 'JS', isDone: true},
+    {id: v1(), title: 'HTMl', isDone: false},
+    {id: v1(), title: 'Redux', isDone: true},
+    {id: v1(), title: 'React', isDone: false},
+  ])
+
+  let [stateSelection, setStateSelection] = useState<SelectionType>('All')
+
+
+  function deleteTaskHandler(id: string) {
+    let filteredTasks = tasks.filter(el => el.id !== id)  //<---пропусти таски id которых не равны id той, которую нужно удалить
+    setTasks(filteredTasks)
+
+  }
+
+  let selection = tasks
+  if (stateSelection === 'Completed') {
+    selection = tasks.filter(el => el.isDone)
+  }
+  if (stateSelection === 'Active') {
+    selection = tasks.filter(el => !el.isDone)
+  }
+
+  const changeSelection = (value: SelectionType) => {
+    setStateSelection(value)
+  };
+
+  function addNewTask (title:string) {
+    let newTask = {id: v1(), title: title, isDone: false}
+    let newTasks = [newTask, ...tasks]
+    setTasks(newTasks)
+
+  }
+
+  const changeStatusTasks =(taskId:string, isDone:boolean)=>{
+    let task = tasks.find((el)=>el.id === taskId)
+    if(task){
+      task.isDone = isDone
+    }
+    // let newTasks = [...tasks]
+    setTasks([...tasks])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Todolist
+        title={'What to learn'}
+        tasks={selection}
+        deleteTask={deleteTaskHandler}
+        changeSelection={changeSelection}
+        addNewTask={addNewTask}
+        changeStatus={changeStatusTasks}
+        filterTask={stateSelection}
+      />
+
+
     </div>
   );
 }
