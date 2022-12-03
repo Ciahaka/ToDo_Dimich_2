c57af1e0fb6aad16e10312c2569401be2c79a1f0
Ciahaka <wilddiver.by@gmail.com>
2022-12-03 20:52:35 +0300

    fix App for Todo #5

--- src/App.tsx
+++ src/App.tsx
@@ -3,61 +3,33 @@ import './App.css';
 import {Todolist} from './component/Todolist';
 import {v1} from 'uuid';
 
-
 export type SelectionType = 'All' | 'Completed' | 'Active'
+
+export type TaskPropsType = {
+  id: string
+  title: string
+  isDone: boolean
+
+}
 export type  TodoListsStateType = {
   id: string
   title: string
-  filterTask: SelectionType
+   filterTask: SelectionType
+}
+type TaskStateType={
+  [todoListsId:string]:Array<TaskPropsType>
 }
 
 function App() {
+  const todoListsId_1 = v1()
+  const todoListsId_2 = v1()
 
-  function deleteTaskHandler(id: string, todoListId: string) {
-    let tasks = tasksObg[todoListId]
-    tasksObg[todoListId] = tasks.filter(el => el.id !== id)  //<---пропусти таски id которых не равны id той, которую нужно удалить
-    setTasks(tasksObg)
-
-  }
-
-  const changeSelection = (value: SelectionType, todoListsId: string) => {
-    let todoList = todoLists.find((tl) => tl.id === todoListsId)
-    if (todoList) {
-      todoList.filterTask = value;
-      setTodoLists([...todoLists])
-    }
-  };
-
-  function addNewTask(title: string, todoListsId: string) {
-    let newTask = {id: v1(), title: title, isDone: false}
-    let tasks = tasksObg[todoListsId]
-    // let newTasks = [newTask, ...tasksObg]   ///избыточная запись для примера. можно от переменной tasksObg[todoListsId] = [newTask, ...tasksObg]
-    tasksObg[todoListsId] = [newTask, ...tasks]
-    setTasks({...tasksObg})
-
-  }
-
-  const changeStatusTasks = (taskId: string, isDone: boolean, todoListsId: string) => {
-    let tasks = tasksObg[todoListsId]
-    let task = tasks.find((el) => el.id === taskId)
-    if (task) {
-      task.isDone = isDone
-      setTasks({...tasksObg})
-    }
-    // let newTasks = [...tasksObg]
-
-
-  }
-  let [todoLists, setTodoLists] = useState<Array<TodoListsStateType>>([
+  const [todoLists, setTodoLists] = useState<Array<TodoListsStateType>>([
     {id: v1(), title: 'What to learn', filterTask: 'All'},
     {id: v1(), title: 'What to buy', filterTask: 'All'},
   ])
 
-  let todoListsId_1 = v1()
-  let todoListsId_2 = v1()
-
-
-  let [tasksObg, setTasks] = useState({
+  const [tasksObg, setTasks] = useState<TaskStateType>({
     [todoListsId_1]: [
       {id: v1(), title: 'JS', isDone: true},
       {id: v1(), title: 'HTMl', isDone: false},
@@ -70,16 +42,52 @@ function App() {
       {id: v1(), title: 'Ku-ku-ru-ka', isDone: true},
     ]
   })
-  let delTodolist = (todoListsId: string) => {
-    let filteredTodo = todoLists.filter(el => el.id !== todoListsId)
+
+  const deleteTaskHandler = (id: string, todoListId: string) => {
+  const tasks = tasksObg[todoListId]
+    tasksObg[todoListId] = tasks.filter(el => el.id !== id)  //<---пропусти таски id которых не равны id той, которую нужно удалить
+    setTasks(tasksObg)
+
+  }
+
+  const changeSelection = (value: SelectionType, todoListsId: string) => {
+    const todoList = todoLists.find((tl) => tl.id === todoListsId)
+    if (todoList) {
+      todoList.filterTask = value;
+      setTodoLists([...todoLists])
+    }
+  };
+
+  const addNewTask = (title: string, todoListsId: string) => {
+    const newTask = {id: v1(), title: title, isDone: false}
+    const tasks = tasksObg[todoListsId]
+    // let newTasks = [newTask, ...tasksObg]   ///избыточная запись для примера. можно от переменной tasksObg[todoListsId] = [newTask, ...tasksObg]
+    tasksObg[todoListsId] = [newTask, ...tasks]
+    setTasks({...tasksObg})
+
+  }
+
+  const changeStatusTasks = (taskId: string, isDone: boolean, todoListsId: string) => {
+    const tasks = tasksObg[todoListsId]
+    const task = tasks.find((el) => el.id === taskId)
+    if (task) {
+      task.isDone = isDone
+      setTasks({...tasksObg})
+    }
+  }
+
+const delTodolist = (todoListsId: string) => {
+   const filteredTodo = todoLists.filter(el => el.id !== todoListsId)
     setTodoLists(filteredTodo)
     delete tasksObg[todoListsId]
     setTasks({...tasksObg})
   }
+
   return (
     <div className="App">
       {todoLists.map((tl) => {
-        let selection = tasksObg[tl.id]
+        debugger
+         let selection = tasksObg[tl.id]
         if (tl.filterTask === 'Completed') {
           selection = selection.filter(el => el.isDone)
         }
