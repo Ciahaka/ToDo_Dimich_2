86227650ed9fb94b97d91c7f836a9f7614fd0020
Ciahaka <wilddiver.by@gmail.com>
2022-12-05 13:51:44 +0300

    add ChangeSpanForm component for Todo #6

---
+++ src/component/ChangeSpanForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export type ChangeSpanFormPropsType ={
+
+
+}
+
+export const ChangeSpanForm = (props:ChangeSpanFormPropsType) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
