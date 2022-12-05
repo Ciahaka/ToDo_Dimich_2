c92455bbf4fbb5655cdaace45b0e8874a6918b05
Ciahaka <wilddiver.by@gmail.com>
2022-12-05 17:58:29 +0300

    fix ChangeSpanForm.tsx

--- src/component/ChangeSpanForm.tsx
+++ src/component/ChangeSpanForm.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import s from './Todolist.module.css';
 
-export type ChangeSpanFormPropsType ={
-
-
+export type ChangeSpanFormPropsType = {
+  title: string
+  changeActiveBlur:(value:string)=>void
 }
 
-export const ChangeSpanForm = (props:ChangeSpanFormPropsType) => {
-  return (
-    <div>
+export const ChangeSpanForm = (props: ChangeSpanFormPropsType) => {
+  const [editMode, setEditMode] = useState(false)
+  const [title, setTitle] = useState('')
+  const addInputModeHandler =()=>{
+    setEditMode(true)
+    setTitle(props.title)
+  }
+const deActiveInputModeHandler = ()=>{
+  setEditMode(false)
+  props.changeActiveBlur(title)
+}
+const changeTitleInputHandler = (e:ChangeEvent<HTMLInputElement>)=>setTitle(e.currentTarget.value)
 
-    </div>
-  );
+  return editMode
+    ? <input value={title} onBlur={deActiveInputModeHandler} autoFocus onChange={changeTitleInputHandler}/>
+    : <span className={s.span} onDoubleClick={addInputModeHandler}>{props.title}</span>;
 };
 
