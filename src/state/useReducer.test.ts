eace706f51335b946ace248969c332fecdb893f3
Ciahaka <wilddiver.by@gmail.com>
2022-12-27 21:58:00 +0300

    finish reducer for practice

--- src/state/useReducer.test.ts
+++ src/state/useReducer.test.ts
@@ -1,16 +1,28 @@
 import React from 'react';
 import {userReducer} from './useReducer';
 
-test('Редьюсер должен увеличить только возраст', ()=>{
-  const startState ={age:25,childrenCount:15,name:'Bobo'};
-  const endState = userReducer(startState, {type:'INCREMENT-AGE'})
+test('Редьюсер должен увеличить только возраст', () => {
+  const startState = {age: 25, childrenCount: 15, name: 'Bobo'};
+  const endState = userReducer(startState, {type: 'INCREMENT-AGE'})
   expect(endState.age).toBe(26)
   expect(endState.childrenCount).toBe(15)
 })
 
-test('Редьюсер должен увеличить только счётчик', () =>{
-  const startState_1 ={age:25,childrenCount:15,name:'Bobo'}
-  const  endState = userReducer(startState_1,{type:'INCREMENT-CHILDREN-COUNT'})
+test('Редьюсер должен увеличить только счётчик', () => {
+  const startState_1 = {age: 25, childrenCount: 15, name: 'Bobo'}
+  const endState = userReducer(startState_1, {type: 'INCREMENT-CHILDREN-COUNT'})
   expect(endState.childrenCount).toBe(16)
   expect(endState.age).toBe(25)
+
 })
+
+test('Reducer измени имя пользователя', () => {
+  const startState_2 = {age: 25, childrenCount: 15, name: 'Bobo'}
+  const endState_2 = userReducer(startState_2, {type: 'CHANGE-USER-NAME'})
+  expect(endState_2.name).toBe('Alex')
+  expect(endState_2.age).toBe(25)
+  expect(endState_2.childrenCount).toBe(15)
+
+})
+
+
--- src/state/useReducer.ts
+++ src/state/useReducer.ts
@@ -11,14 +11,20 @@ export type ActionType = {
   [key: string]: any
 }
 
-export const userReducer = (state:StateType,action:ActionType) => {
-  switch (action.type){
+export const userReducer = (state: StateType, action: ActionType): StateType => {
+  // const newState = {...state}
+  switch (action.type) {
     case 'INCREMENT-AGE':
-      state.age = state.age + 1;
-      return state
+      // newState.age = state.age + 1;
+      // return newState
+      return {...state, age: state.age + 1}
     case 'INCREMENT-CHILDREN-COUNT':
-      state.childrenCount = state.childrenCount + 1
-      return state
-    default: throw new Error('Спасите! Не знаю, что делать!')
+      // newState.childrenCount = state.childrenCount + 1
+      // return newState
+      return {...state, childrenCount: state.childrenCount + 1}
+    case 'CHANGE-USER-NAME':
+      return {...state, name: state.name = 'Alex'}
+    default:
+      throw new Error('Спасите! Не знаю, что делать!')
   }
 }
